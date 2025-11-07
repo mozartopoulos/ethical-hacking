d9623f160f1d439f9caaec766721a0b76fd982ed
Ilias Spiliopoulos <iliasatmim@gmail.com>
2025-11-07 23:31:50 +0100

    Update enumeration-cheatsheet.sh

--- notes/enumeration-cheatsheet.sh
+++ notes/enumeration-cheatsheet.sh
@@ -6,6 +6,10 @@ netdiscover -i eth0 -r 10.0.0.1/24
 #if it doesn't work (no eth for example) then try ping scan on the whole network
 ifconfig
 nmap 10.0.0.1/24 -T5 -v
+
+#check also other routes that you might have with just a
+route
+
 # ------------------------- 
 # Install Rustscan!
 # Download the .deb file from the releases page:
